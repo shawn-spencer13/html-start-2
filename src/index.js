5bcaca7365f1b0978e3d8bfc0c66d579cee78776
shawn-spencer13 <68129782+shawn-spencer13@users.noreply.github.com>
2020-07-15 16:17:56 -0500

    js cool color thing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import "./styles.css";
 // javscript below here
 const page2Button = document.getElementById("page2Button");
 const rangeInput = document.getElementById("rangeInput");
+const colorinput = document.getElementById("color");
+
+colorinput.addEventListener("input", e => {
+  console.log(e.target.value);
+  document.body.style.background = e.target.value;
+});
 
 page2Button.addEventListener("click", () => {
   window.location.href = "/other.html";
 });
 
-rangeInput.addEventListener("change", e => {
-  e.preventDefault();
-  console.log(e.target.value);
+rangeInput.addEventListener("input", e => {
+  // console.log(e.target.value);
+  const rangevalue = document.getElementById("rangevalue");
+
+  rangevalue.innerHTML = e.target.value;
 });
